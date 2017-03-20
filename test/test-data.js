9693859ea69efc8128a3bcc2e6bc6495f3c4111b
Mohammad Ali Khan <mohammad_ali93@live.com>
2017-03-20 13:19:10 +0000

    Add data-to-graph conversion functions for library
    This change adds basic versions of functions used to convert data received into
    chartJS compatible data and displaying it. This has been structured into
    different files with some test data for now. External libraries in the test
    folder are included via CDN for now but will changed in the future.

---
+++ test/test-data.js
@@ -0,0 +1,74 @@
+testData = [{
+  "timestamp": "2016-12-18T14:58:54.026Z",
+  "outcomes": [{
+    "outcome": "Happiness",
+    "value": 8,
+    "notes": "I am pretty happy"
+  }, {
+    "outcome": "Loneliness",
+    "value": 1,
+    "notes": "I am isolated"
+  }, {
+    "outcome": "Health",
+    "value": 5
+  }, {
+    "outcome": "Managing at Home",
+    "value": 2
+  }, {
+    "outcome": "Finances",
+    "value": 4
+  }, {
+    "outcome": "Work",
+    "value": 10
+  }],
+  "notes": "Agreed the session went well"
+}, {
+  "timestamp": "2017-01-18T20:58:54.026Z",
+  "outcomes": [{
+    "outcome": "Happiness",
+    "value": 10,
+    "notes": "Joined social club"
+  }, {
+    "outcome": "Loneliness",
+    "value": 3
+  }, {
+    "outcome": "Health",
+    "value": 4
+  }, {
+    "outcome": "Managing at Home",
+    "value": 3
+  }, {
+    "outcome": "Finances",
+    "value": 5
+  }, {
+    "outcome": "Work",
+    "value": 8,
+    "notes": "Was late one day"
+  }]
+}, {
+  "timestamp": "2017-02-15T08:58:54.026Z",
+  "outcomes": [{
+    "outcome": "Happiness",
+    "value": 10
+  }, {
+    "outcome": "Loneliness",
+    "value": 4
+  }, {
+    "outcome": "Health",
+    "value": 5
+  }, {
+    "outcome": "Managing at Home",
+    "value": 4
+  }, {
+    "outcome": "Finances",
+    "value": 8,
+    "notes": "Won at bingo"
+  }, {
+    "outcome": "Work",
+    "value": 9
+  }, {
+    "outcome": "Relationships",
+    "value": 7
+  }],
+  "notes": "Discharged"
+}]
