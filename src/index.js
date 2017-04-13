7d2b1f2c9fa7260c9f30c1b86d9887966b8c5fd3
themakshter <mohammad_ali93@live.com>
2017-04-13 21:36:09 +0100

    Create sessionsConverter module
    The method to convert sessions data to ChartJS data has been converted
    into a proper module, following the module design pattern. The demo code
    has also been changed to use this module, with success.

--- src/index.js
+++ src/index.js
@@ -4,70 +4,6 @@ var moment = require('moment');
 var Chart = require('chart.js');
 var randomColor = requrie('randomcolor');
 
-function convertToChartData(sessions){
-  //need to know labels
-  var labels = [];
-  var labelSet = {};
-  var convertedData = [];
-  var now = moment();
-  var color = Chart.helpers.color;
-    // go over each session
-  sessions.forEach(function(session){
-    var outcomes = session.outcomes;
-    // data set item for session
-    var convertedSession = {};
-
-    var momentTimestamp = moment(session.timestamp);
-    var humanisedTimestamp = moment.duration(momentTimestamp.diff(now)).humanize(true);
-
-    convertedSession.label = humanisedTimestamp;
-    convertedSession.dataMap = {};
-    convertedSession.noteMap = {};
-    // add each outcome to a set
-    outcomes.forEach(function(outcome){
-      // check if our set has the value or not
-      var lowerCaseLabel = outcome.outcome.toLowerCase();
-      if(!labelSet.hasOwnProperty(lowerCaseLabel)){
-        labels.push(outcome.outcome);
-        // now value is in our set
-        labelSet[lowerCaseLabel] = true;
-      }
-      convertedSession.dataMap[lowerCaseLabel] = outcome.value;
-      convertedSession.noteMap[lowerCaseLabel] = outcome.notes;
-    });
-    convertedSession.data = [];
-    convertedSession.notes = [];
-    // loop over our labels to get the data for each and create the data array
-    labels.forEach(function(label){
-      var dataMapValue = convertedSession.dataMap[label.toLowerCase()];
-      var noteMapValue = convertedSession.noteMap[label.toLowerCase()];
-      if(dataMapValue !== undefined){
-        convertedSession.data.push(dataMapValue);
-      }else{
-        convertedSession.data.push(null);
-      }
-
-      if(noteMapValue !== undefined){
-        convertedSession.notes.push(noteMapValue);
-      }else{
-        convertedSession.notes.push("none");
-      }
-      //assign random colour to chart
-      var colour = randomColor({
-        format: 'rgba'
-      });
-      convertedSession.backgroundColor = color(colour).alpha(0.2).rgbString();
-      convertedSession.borderColor = colour;
-      convertedSession.pointBackgroundColor = colour;
-    });
-    convertedData.push(convertedSession);
-  });
-  var chartData = {};
-  chartData.labels = labels;
-  chartData.datasets = convertedData;
-  return chartData;
-}
-
 function createOutcomeGraph(div, title, data){
   var chartConfig = createConfig(data,title);
   return new Chart(document.getElementById(div), chartConfig);
@@ -76,7 +12,7 @@ function createOutcomeGraph(div, title, data){
 function createConfig(data, title){
   return {
     type: 'radar',
-    data: convertToChartData(data),
+    data: sessionsConverter.getChartJSConvertedData(data),
     options: {
       legend: {
         position: 'top',
---
+++ src/sessionsConverter.js
@@ -0,0 +1,114 @@
+// module pattern
+var sessionsConverter = (function(){
+  // variable to keep track of different labels
+  var labels = [];
+  // labels set for checking if label is unique
+  var labelSet = {};
+
+  function init(){
+    labels = [];
+    labelSet = {};
+  }
+
+  function getConvertedSessions(sessions){
+    var convertedSessions = [];
+    sessions.forEach(function(session){
+      convertedSessions.push(getConvertedSession(session));
+    });
+    return convertedSessions;
+  }
+
+  function getConvertedSession(session){
+    var now = moment(); // current time
+
+    var outcomes = session.outcomes;
+    // data set item for session
+    var convertedSession = {};
+
+    var momentTimestamp = moment(session.timestamp);
+    var humanisedTimestamp = moment.duration(momentTimestamp.diff(now)).humanize(true);
+
+    convertedSession.label = humanisedTimestamp;
+
+    convertedSession.valuesMap = getValuesMapsFromOutcomes(outcomes);
+
+    // extract data and notes arrays
+    var extractedDataAndNotes = getExtractedDataAndTooltipNotes(convertedSession.valuesMap);
+    convertedSession.data = extractedDataAndNotes.data;
+    convertedSession.notes = extractedDataAndNotes.notes;
+
+
+    var color = Chart.helpers.color;
+    //assign random colour to chart
+    var colour = randomColor({
+      format: 'rgba'
+    });
+    convertedSession.backgroundColor = color(colour).alpha(0.2).rgbString();
+    convertedSession.borderColor = colour;
+    convertedSession.pointBackgroundColor = colour;
+    
+    return convertedSession;
+  }
+
+  function getValuesMapsFromOutcomes(outcomes){
+    var dataMap = {};
+    var noteMap = {};
+    // add each outcome to a set
+    outcomes.forEach(function(outcome){
+      var lowerCaseLabel = outcome.outcome.toLowerCase();
+      updateLabelSet(outcome.outcome);
+      dataMap[lowerCaseLabel] = outcome.value;
+      noteMap[lowerCaseLabel] = outcome.notes;
+    });
+    return {
+      data : dataMap,
+      notes: noteMap
+    }
+  }
+
+  // check if our set has the value or not
+  function updateLabelSet(potentialLabel){
+    var lowerCaseLabel = potentialLabel.toLowerCase();
+    if(!labelSet.hasOwnProperty(lowerCaseLabel)){
+      labels.push(potentialLabel);
+      // now value is in our set
+      labelSet[lowerCaseLabel] = true;
+    }
+  }
+
+  function getExtractedDataAndTooltipNotes(valuesMap){
+    // go over currently added labels
+    var data = [];
+    var notes = [];
+    labels.forEach(function(label){
+      data.push(getExtractedDataValue(valuesMap.data[label.toLowerCase()]));
+      notes.push(getExtractedNoteValue(valuesMap.notes[label.toLowerCase()]))
+    });
+    return {
+      data : data,
+      notes: notes
+    }
+  }
+
+  function getExtractedDataValue(dataValue){
+    return dataValue === undefined ? null : dataValue;
+  }
+
+  function getExtractedNoteValue(noteValue){
+    return noteValue === undefined ? "none" : noteValue; 
+  }
+
+  function getLabels(){
+    return labels;
+  }
+
+  return {
+    getChartJSConvertedData: function(sessions){
+      init();
+      var chartData = {};
+      chartData.datasets = getConvertedSessions(sessions);
+      chartData.labels = getLabels();
+      return chartData;
+    }
+  };
+})();
